c0aff9c39e1946aaecb9c89dd9c719aaf47ee482
panospd <anastapd@gmail.com>
2020-12-28 19:49:14 +0000

    redux devtools setup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch(bugAdded("Bug1"));
+// store.dispatch(bugAdded("Bug1"));
+// store.dispatch(bugAdded("Bug2"));
+// store.dispatch(bugAdded("Bug3"));
 
-store.dispatch(bugResolved(1));
-
-store.dispatch(bugRemoved(1));
+// store.dispatch(bugResolved(1));
 
 console.log(store.getState());
