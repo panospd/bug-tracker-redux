6c05042250be2f36976dc72ab64bc158e5235cb8
panospd <anastapd@gmail.com>
2020-12-31 10:29:44 +0000

    Implemented redrux middleware

--- src/index.js
+++ src/index.js
@@ -11,23 +11,28 @@ import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-store.dispatch(userAdded({ name: "Panos" }));
-store.dispatch(userAdded({ name: "Chris" }));
+// store.dispatch(userAdded({ name: "Panos" }));
+// store.dispatch(userAdded({ name: "Chris" }));
 
-store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
+  console.log(getState());
+});
 
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-store.dispatch(bugResolved({ id: 1 }));
+store.dispatch({
+  type: "error",
+  payload: {
+    message: "An error occured",
+  },
+});
 
 const bugs = getBugsByUser(1)(store.getState());
 
-console.log(bugs);
-
 const unresolvedBugs = getUnresolvedBugs(store.getState());
-
-console.log(unresolvedBugs);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 import reducer from "./reducer";
 
 export default function () {
   return configureStore({
     reducer,
+    middleware: [...getDefaultMiddleware(), logger("console"), toast],
   });
 }
