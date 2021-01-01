d9b7537e334c524c1e8c5191fdd17244914d534f
panospd <anastapd@gmail.com>
2021-01-01 13:54:03 +0000

    Implemented saving and updating to server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import {
-  bugAdded,
-  bugResolved,
-  getUnresolvedBugs,
-  bugAssignedToUser,
-  getBugsByUser,
-} from "./store/bugs";
-import { projectAdded } from "./store/projects";
-import { userAdded } from "./store/users";
+import { loadBugs, assignBugToUser } from "./store/bugs";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
+// store.dispatch(loadBugs());
+
+store.dispatch(loadBugs());
+
+setTimeout(() => {
+  store.dispatch(assignBugToUser(1, 4));
+}, 2000);
 
 // store.dispatch(userAdded({ name: "Panos" }));
 // store.dispatch(userAdded({ name: "Chris" }));
@@ -21,18 +20,18 @@ const store = configureStore();
 // store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 // store.dispatch(bugResolved({ id: 1 }));
 
-store.dispatch((dispatch, getState) => {
-  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
-  console.log(getState());
-});
+// store.dispatch((dispatch, getState) => {
+//   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
+//   console.log(getState());
+// });
 
-store.dispatch({
-  type: "error",
-  payload: {
-    message: "An error occured",
-  },
-});
+// store.dispatch({
+//   type: "error",
+//   payload: {
+//     message: "An error occured",
+//   },
+// });
 
-const bugs = getBugsByUser(1)(store.getState());
+// const bugs = getBugsByUser(1)(store.getState());
 
-const unresolvedBugs = getUnresolvedBugs(store.getState());
+// const unresolvedBugs = getUnresolvedBugs(store.getState());
